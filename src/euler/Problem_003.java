df6b834700f82d3f2d6cae2b06d94057e3c7ba8e
Adamknaggs <adamknaggs16@hotmail.com>
2014-03-11 11:46:23 +0000

    problem three is here

---
+++ src/euler/Problem_003.java
@@ -0,0 +1,16 @@
+package euler;
+
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * 
+ * What is the largest prime factor of the number 600851475143 ?
+ * 
+ * @author adamknaggs
+ * 
+ */
+public class Problem_003 {
+
+	public static void main(String[] args) {
+
+	}
+}
